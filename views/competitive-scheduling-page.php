4dabf50ca64abebf6e9370f4985482e0b40d7e23
Otávio - Casa <otavio@b2make.com>
2023-10-03 14:29:03 -0300

    escapamento do título

--- views/competitive-scheduling-page.php
+++ views/competitive-scheduling-page.php
@@ -1,2 +1,2 @@
-<h3>Competitive Scheduling</h3>
+<h1 class="wp-heading-inline"><?php esc_html__( 'Competitive Scheduling', 'competitive-scheduling' ) ?></h1>
 <div class="teste">Teste de conteúdo. Novo. 20</div>
