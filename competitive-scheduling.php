6ce4f4a287c5f5ed0b97b7b83adec55b028601f3
Otávio - Casa <otavio@b2make.com>
2023-10-03 14:24:45 -0300

    ALteração do filemtime do css

--- competitive-scheduling.php
+++ competitive-scheduling.php
@@ -109,7 +109,7 @@ if( ! class_exists( 'Competitive_Scheduling' ) ){
                 return;
             }
 
-            wp_enqueue_style( 'competitive-scheduling-admin', COMP_SCHEDULE_URL . 'assets/css/admin.css' );
+            wp_enqueue_style( 'competitive-scheduling-admin', COMP_SCHEDULE_URL . 'assets/css/admin.css', arrya(  ), ( COMP_SCHEDULE_DEBUG ? filemtime( COMP_SCHEDULE_PATH . 'assets/css/admin.css' ) : COMP_SCHEDULE_VERSION ) );
             wp_enqueue_script( 'competitive-scheduling-admin', COMP_SCHEDULE_URL . 'assets/js/admin.js', array( 'jquery' ), ( COMP_SCHEDULE_DEBUG ? filemtime( COMP_SCHEDULE_PATH . 'assets/js/admin.js' ) : COMP_SCHEDULE_VERSION ) );
 
             require( COMP_SCHEDULE_PATH . 'views/competitive-scheduling-page.php' );
