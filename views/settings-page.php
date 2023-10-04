99796457d384267e6ea508893e91eb71d58c0f5e
Otávio - Casa <otavio@b2make.com>
2023-10-04 09:59:26 -0300

    update settings-page.php

--- views/settings-page.php
+++ views/settings-page.php
@@ -1,6 +1,10 @@
 <div class="wrap">
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
     <form action="options.php" method="post">
-        
+    <?php 
+        settings_fields( 'competitive_scheduling_group' );
+        do_settings_sections( 'competitive_scheduling_page1' );
+        submit_button( esc_html__( 'Save Settings', 'competitive-scheduling' ) );
+    ?>
     </form>
 </div>
