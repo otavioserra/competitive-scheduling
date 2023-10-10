7437f784fb31a0cd196d96d22646c6b57a692532
Otávio - Casa <otavio@b2make.com>
2023-10-10 16:20:26 -0300

    update class.competitive-scheduling-settings.php and settings-page.php

--- settings/class.competitive-scheduling-settings.php
+++ settings/class.competitive-scheduling-settings.php
@@ -36,32 +36,49 @@ if( !class_exists( 'Competitive_Scheduling_Settings' ) ){
 
             add_settings_section(
                 'competitive_scheduling_main_section',
-                esc_html__( 'How does it work?', 'competitive-scheduling' ),
-                array( $this, 'main_section_callback' ),
-                'competitive_scheduling_page1'
+                esc_html__( 'Main', 'competitive-scheduling' ),
+                array( $this, 'section_callback_main' ),
+                'competitive_scheduling_main'
             );
 
+            $this->section_fields_main();
+
+            add_settings_section(
+                'competitive_scheduling_email_section',
+                esc_html__( 'Main', 'competitive-scheduling' ),
+                array( $this, 'section_callback_email' ),
+                'competitive_scheduling_email'
+            );
+
+            $this->section_fields_email();
+        }
+
+        function section_callback_main() {
+            esc_html_e( 'Here you can find all the main options to personalise your plugin\'s instance.', 'competitive-scheduling' );
+        }
+
+        function section_fields_main(){
             add_settings_field(
                 'activation',
                 esc_html__( 'Activation', 'competitive-scheduling' ),
-                array( $this, 'activation_callback' ),
-                'competitive_scheduling_page1',
+                array( $this, 'field_activation_callback' ),
+                'competitive_scheduling_main',
                 'competitive_scheduling_main_section'
             );
 
             add_settings_field(
                 'shortcode',
                 esc_html__( 'Shortcode', 'competitive-scheduling' ),
-                array( $this, 'shortcode_callback' ),
-                'competitive_scheduling_page1',
+                array( $this, 'field_shortcode_callback' ),
+                'competitive_scheduling_main',
                 'competitive_scheduling_main_section'
             );
 
             add_settings_field(
                 'subject',
                 esc_html__( 'Subject', 'competitive-scheduling' ),
-                array( $this, 'subject_callback' ),
-                'competitive_scheduling_page1',
+                array( $this, 'field_subject_callback' ),
+                'competitive_scheduling_main',
                 'competitive_scheduling_main_section',
                 array(
                     'label_for' => 'subject'
@@ -69,11 +86,21 @@ if( !class_exists( 'Competitive_Scheduling_Settings' ) ){
             );
         }
 
-        function main_section_callback() {
-            esc_html_e('Here you can find all the main options to personalise your plugin\'s instance.','competitive-scheduling');
+        function section_callback_email() {
+            esc_html_e( 'Here you can find all the email options to personalise your plugin\'s instance.', 'competitive-scheduling' );
         }
 
-        public function activation_callback(){
+        function section_fields_email(){
+            add_settings_field(
+                'message',
+                esc_html__( 'Activation', 'competitive-scheduling' ),
+                array( $this, 'field_message_callback' ),
+                'competitive_scheduling_email',
+                'competitive_scheduling_email_section'
+            );
+        }
+
+        public function field_activation_callback(){
             ?>
 
                 <input 
@@ -91,13 +118,26 @@ if( !class_exists( 'Competitive_Scheduling_Settings' ) ){
             <?php
         }
 
-        public function shortcode_callback(){
+        public function field_shortcode_callback(){
             ?>
             <span><?php echo esc_html__( 'Use the shortcode [competitive_scheduling] to display the controler in any page/post/widget. IMPORTANT: is necessary to be logged-in to see it.', 'competitive-scheduling' ); ?></span>
             <?php
         }
 
-        public function subject_callback(){
+        public function field_subject_callback(){
+            ?>
+                <input 
+                type="text" 
+                name="competitive_scheduling_options[subject]" 
+                id="subject"
+                class="input-titles" 
+                value="<?php echo isset( self::$options['subject'] ) ? esc_attr( self::$options['subject'] ) : ''; ?>"
+                >
+                <p><?php echo esc_html__( 'Subject of emails that will be sent to users\' appointments made on your website.', 'competitive-scheduling' ); ?></p> 
+            <?php
+        }
+
+        public function field_message_callback(){
             ?>
                 <input 
                 type="text" 
--- views/settings-page.php
+++ views/settings-page.php
@@ -1,9 +1,27 @@
 <div class="wrap">
-    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    <?php 
+        $active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'main';
+    ?>
+    <h2 class="nav-tab-wrapper">
+        <a href="?page=competitive_scheduling_settings&tab=main" class="nav-tab <?php echo $active_tab == 'main' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Main', 'competitive-scheduling' ); ?></a>
+        <a href="?page=competitive_scheduling_settings&tab=email" class="nav-tab <?php echo $active_tab == 'email' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Email', 'competitive-scheduling' ); ?></a>
+    </h2>
     <form action="options.php" method="post">
     <?php 
         settings_fields( 'competitive_scheduling_group' );
-        do_settings_sections( 'competitive_scheduling_page1' );
+
+        switch($active_tab) {
+            case 'main':
+                do_settings_sections( 'competitive_scheduling_main' );
+            break;
+            case 'email':
+                do_settings_sections( 'competitive_scheduling_email' );
+            break;
+            default:
+                do_settings_sections( 'competitive_scheduling_main' );
+        }
+
         submit_button( esc_html__( 'Save Settings', 'competitive-scheduling' ) );
     ?>
     </form>
