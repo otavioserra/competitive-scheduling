dec1226bd7c7472a3ec83e9f57f43adb3ef9eaf8
Otávio - Casa <otavio@b2make.com>
2023-10-25 18:19:13 -0300

    update class.competitive-scheduling-priority-coupon-cpt.php

--- post-types/class.competitive-scheduling-priority-coupon-cpt.php
+++ post-types/class.competitive-scheduling-priority-coupon-cpt.php
@@ -67,8 +67,8 @@ if( !class_exists( 'Competitive_Scheduling_Priority_Coupon_Post_Type') ){
                     break;
             }
 
-            echo 'Ação: '.$action;
-
+            $acao = 'Ação: '.$action. ' - Status: ' . $post->post_status;
+            if(WP_DEBUG_LOG) error_log( COMP_SCHEDULE_ID . ': ' . $acao );
         }
 
     }
