7c028d77071194bc1f206afaf0c9ba588e5020fb
Otávio - Casa <otavio@b2make.com>
2024-01-15 18:39:25 -0300

    update class.competitive-scheduling-shortcode.php

--- shortcodes/class.competitive-scheduling-shortcode.php
+++ shortcodes/class.competitive-scheduling-shortcode.php
@@ -163,14 +163,17 @@ if( ! class_exists( 'Competitive_Scheduling_Shortcode' ) ){
         }
 
         private function shortcode_page( $page ){
+            $cron_jobs = get_option( 'cron' );
+            var_dump($cron_jobs);
+            
             $proxima_execucao = wp_get_scheduled_event( 'competitive_scheduling_cron_hook' );
             echo 'Mensagem: competitive_scheduling_cron_hook' . '<br>';
             echo 'proxima_execucao: ' . print_r( $proxima_execucao, true ) . '<br>';
             echo 'Forçar execução: competitive_scheduling_cron_hook' . '<br>';
             do_action( 'competitive_scheduling_cron_hook' );
 
-            require_once( CS_PATH . 'includes/class.cron.php' );
-            Cron::run();
+            /* require_once( CS_PATH . 'includes/class.cron.php' );
+            Cron::run(); */
 
             exit;
 
