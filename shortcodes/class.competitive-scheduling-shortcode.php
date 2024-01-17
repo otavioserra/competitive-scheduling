6cff337c52925111f7286316f7a85cf14a2ee9ca
Otávio - Casa <otavio@b2make.com>
2024-01-17 17:09:55 -0300

    update class.competitive-scheduling-shortcode.php

--- shortcodes/class.competitive-scheduling-shortcode.php
+++ shortcodes/class.competitive-scheduling-shortcode.php
@@ -163,9 +163,11 @@ if( ! class_exists( 'Competitive_Scheduling_Shortcode' ) ){
         }
 
         private function shortcode_page( $page ){
-            echo 'Rodar tests: ' . '<br>';
             require_once( CS_PATH . 'includes/class.cron.php' );
-            Cron::tests();
+            /* echo 'Rodar tests(): ' . '<br>';
+            Cron::tests(); */
+            echo 'Rodar run(): ' . '<br>';
+            Cron::run();
 
             exit;
 
