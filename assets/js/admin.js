d24c12841dc5883801b270f6389a9e698fae4516
Otávio - Casa <otavio@b2make.com>
2024-03-25 14:43:58 -0300

    update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -253,10 +253,8 @@ jQuery( document ).ready( function(){
 				// Print the stylesheet on the page.
 				var popupWindow = window.open(manager.printTitle + '.pdf', 'Print', 'menubar=0,location=0,width=600,height=400');
 
-				// Set the page title in the print window.
-				popupWindow.document.write('<title>'+manager.printTitle+'</title>');
-				popupWindow.document.write('<style>'+newCss+'</style>');
-				popupWindow.document.write(document.getElementById('popup-content').innerHTML);
+				// Set the page in the print window.
+				popupWindow.document.write('<!doctype html><html><head><title>'+manager.printTitle+'</title><style>'+newCss+'</style></head><body>'+document.getElementById('popup-content').innerHTML+'</body></html>');
 
 				// Start printing.
 				popupWindow.print();
