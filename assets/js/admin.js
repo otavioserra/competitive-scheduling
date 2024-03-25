9e89201abd0bd304ec4e993b7065c2d17ad49419
Otávio - Casa <otavio@b2make.com>
2024-03-25 15:01:40 -0300

    update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -255,7 +255,7 @@ jQuery( document ).ready( function(){
 				// Print the stylesheet on the page.
 				var popupWindow = window.open('//'+manager.printTitle + '.pdf', 'Print', 'menubar=0,location=0,width=600,height=400');
 
-				console.log(popupWindow.name);
+				/* console.log(popupWindow.name);
 
 				popupWindow.name = manager.printTitle;
 
@@ -263,7 +263,7 @@ jQuery( document ).ready( function(){
 				popupWindow.document.write('<!doctype html><html><head><title>'+manager.printTitle+'</title><script>window.name="'+manager.printTitle+'";</script><style>'+newCss+'</style></head><body>'+document.getElementById('popup-content').innerHTML+'</body></html>');
 
 				// Start printing.
-				popupWindow.print();
+				popupWindow.print(); */
 			} else {
 				console.log('Error loading stylesheet');
 			}
