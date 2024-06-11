506c74b542aef0263cc1e16f3049641d392c9754
Otávio - Casa <otavio@b2make.com>
2024-06-11 10:28:29 -0300

    update settings-page.php

--- views/settings-page.php
+++ views/settings-page.php
@@ -7,6 +7,7 @@
         <a href="?page=competitive_scheduling_settings&tab=main" class="nav-tab <?php echo $active_tab == 'main' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Main', 'competitive-scheduling' ); ?></a>
         <a href="?page=competitive_scheduling_settings&tab=email" class="nav-tab <?php echo $active_tab == 'email' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Emails', 'competitive-scheduling' ); ?></a>
         <a href="?page=competitive_scheduling_settings&tab=message" class="nav-tab <?php echo $active_tab == 'message' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Messages', 'competitive-scheduling' ); ?></a>
+        <a href="?page=competitive_scheduling_settings&tab=pages" class="nav-tab <?php echo $active_tab == 'pages' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Pages', 'competitive-scheduling' ); ?></a>
         <a href="?page=competitive_scheduling_settings&tab=tools" class="nav-tab <?php echo $active_tab == 'tools' ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Tools', 'competitive-scheduling' ); ?></a>
     </h2>
     <form action="options.php" method="post">
@@ -28,6 +29,10 @@
             case 'tools':
                 settings_fields( 'competitive_scheduling_group_tools' );
                 do_settings_sections( 'competitive_scheduling_tools' );
+            break;
+            case 'pages':
+                settings_fields( 'competitive_scheduling_group_pages_options' );
+                do_settings_sections( 'competitive_scheduling_pages' );
             break;
             default:
                 settings_fields( 'competitive_scheduling_group_options' );
