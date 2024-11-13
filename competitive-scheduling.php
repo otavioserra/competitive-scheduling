c39e3a288d48890b89d7340abbbd7190b424aea2
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2024-11-13 14:03:27 -0300

    update competitive-scheduling.php

--- competitive-scheduling.php
+++ competitive-scheduling.php
@@ -70,10 +70,10 @@ if( ! class_exists( 'Competitive_Scheduling' ) ){
             define( 'CS_URL', plugin_dir_url( __FILE__ ) );
             define( 'CS_URL_FOMANTIC_UI', plugin_dir_url( __FILE__ ) . 'vendor/fomantic-UI@2.9.0/dist/');
             define( 'CS_VERSION', '1.1.4' );
-            define( 'CS_EMAIL_ACTIVE', true );
-            define( 'CS_DEBUG', false );
-            define( 'CS_FORCE_DATE_TODAY', false );
-            define( 'CS_DATE_TODAY_FORCED_VALUE', '2024-06-25' );
+            define( 'CS_EMAIL_ACTIVE', false );
+            define( 'CS_DEBUG', true );
+            define( 'CS_FORCE_DATE_TODAY', true );
+            define( 'CS_DATE_TODAY_FORCED_VALUE', '2024-11-15' );
             define( 'CS_NOUNCE_SCHEDULES', 'cs-nouce-schedules' );
             define( 'CS_NOUNCE_SCHEDULES_EXPIRES', 86400*45 );
             define( 'CS_NUM_RECORDS_PER_PAGE', 20 );
