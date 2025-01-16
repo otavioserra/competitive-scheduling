22edd204deba816184ea53e8e9757b6e4cf40edb
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-01-16 17:06:27 -0300

    create 7 files, update 1 file and delete 2 files

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks'), 'version' => 'f34eb17a7296cff58656');
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { registerBlockType } from "@wordpress/blocks";
+
+registerBlockType("competitive-scheduling/client", {
+    edit: function () {
+        return <p className="teste">Edit</p>;
+    },
+    save: function () {
+        return <p className="teste">Save</p>;
+    },
+});
