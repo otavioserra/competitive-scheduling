0176dd6e378d80c6339d526622c702753b779440
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-01-22 17:18:07 -0300

    feat: Add initial block files
    This commit adds the initial files for the Competitive Scheduling Gutenberg block:
    
    - index.js: registers the block
    - edit.js: defines the editor view
    - save.js: defines the front-end view (currently returns null)
    - style.scss: empty stylesheet for front-end
    - editor.scss: empty stylesheet for editor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '5988643f92d516042d2d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '6e69a2271457f6376905');
--- src/edit.js
+++ src/edit.js
@@ -2,8 +2,6 @@ import { useBlockProps } from '@wordpress/block-editor';
 import './style.scss';
 import './editor.scss';
 
-// const x = 0;
-
 export default function Edit() {
 	const blockProps = useBlockProps();
 	return <p {...blockProps}>Edit JSX</p>;
