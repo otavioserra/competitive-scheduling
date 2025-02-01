3b1f0f4a314ed91998f5daf4688971929485b787
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-02-01 11:24:19 -0300

    update build/index.asset.php and build/index.js.map update 5 files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5cefa8998b556446ee6f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b075013bcd768a7e3ff1');
